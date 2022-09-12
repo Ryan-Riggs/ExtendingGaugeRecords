45d5651595c93457debb17023eb4fbc152b6438a
Ryan-Riggs <54599739+Ryan-Riggs@users.noreply.github.com>
2022-09-12 10:43:16 -0500

    Update and rename applyingModel.R to ExtendingRecords.R

--- Rating curves/ExtendingRecords.R
+++ Rating curves/ExtendingRecords.R
@@ -1,3 +1,8 @@
+##Author: Ryan Riggs
+##Date: 9/12/2022
+##########################################################################################################
+##Libraries
+##########################################################################################################
 library(sf)
 library(raster)
 library(spatial)
@@ -5,8 +10,20 @@ library(RColorBrewer)
 library(ggplot2)
 library(data.table)
 library(dplyr)
-##Read in files and filter to max nse value. 
-files = list.files("E:\\research\\RatingCurveAnalysis\\RatingCurveResultsV6wDistance", full.names=TRUE)
+library(caret)
+library(segmented)
+library(ga)
+library(mgcv)
+library(BBmisc)
+library(hydroGOF)
+library(hydroGOF)
+library(randomForest)
+library(GA)
+library(dplyr)
+#########################################################################################################
+##Read in files and filter to max kge value. 
+#########################################################################################################
+files = list.files("path\\to\\RatingCurveResultsV6wDistance", full.names=TRUE)
 outputList = list()
 for(i in 1:length(files)){
   print(i)
@@ -49,18 +66,6 @@ out$kge = as.numeric(out$kge)
 #######################################################################################
 ##Functions to actually fill in the gauge record. 
 #######################################################################################
-library(caret)
-library(segmented)
-library(ga)
-library(mgcv)
-library(BBmisc)
-library(hydroGOF)
-library(hydroGOF)
-library(randomForest)
-library(GA)
-library(dplyr)
-
-
 names = c("rrmse", "nse", "kge", "nrmse", "rbias", "bias", "rmse", "stde")
 pal = rainbow(8)
 
@@ -82,6 +87,9 @@ validation = function(sim, obs){
   return(df)
 }
 
+#####################################################################################
+##Rating curve functions. 
+#####################################################################################
 mspline<-function(x,y,k=10,lower=NA,upper=NA){
   #fits a monotonic spline to data
   #small values of k= more smoothing (flatter curves)
@@ -115,7 +123,7 @@ predict.mspline<-function(msp,x){
 
 rc_function = function(Train, Valid, alg, total){
   
-  ##RF:We added in max node argument to try and limit overfitting.
+  ##Random Forest
   if(alg=="RF"){
     rf = randomForest(Q ~width,data = Train, ntree= nrow(Train),maxnodes = round(nrow(Train)*.05), mtry = 1)
     out = predict(rf, Valid)
@@ -123,7 +131,7 @@ rc_function = function(Train, Valid, alg, total){
     entire = predict(rf, total)
   }
   
-  ##SPline. 
+  ##Spline. 
   if(alg=="Spl"){
     #spline_test = try(smooth.spline(Train$width, Train$Q, spar = 0.9))
     #spl_preds = predict(spline_test, Valid$width)
@@ -135,7 +143,7 @@ rc_function = function(Train, Valid, alg, total){
     entire = predict.mspline(fv, total$width)
   }
   
-  ##Linear segmentation. 
+  ##piecewise linear regression.
   if(alg=="Pcw"){
     splitFun = function(a){
       reg = lm(Q~width+I((width-a)*(width>=a)),
@@ -230,7 +238,9 @@ rc_function = function(Train, Valid, alg, total){
   entire =ifelse(entire<0, NA, entire)
   return(list(output, entire))
 }
-
+###########################################################################################################################
+##Developing rating curves. 
+###########################################################################################################################
 outer = function(xsec, gage){
   #paired_df = fread(xsection_files[i])
   paired_df = fread(xsec)
@@ -252,13 +262,10 @@ outer = function(xsec, gage){
     paired_df = inner_join(paired_df, usgs_q)
     paired_df$Q = paired_df$q
     paired_df = paired_df[!is.na(paired_df$Q),]
-    #########################################################################################################    
     nodes = unique(paired_df$node_id)
     return(list(paired_df, nodes))
   }}
 
-#####Double check inner function to ensure that no duplicates are made and that performance is the same. 
-###############
 inner = function(paired_df,nodes){      
   paired_df_1 = paired_df[.(nodes)]
   agg = aggregate(width ~ Date, paired_df_1, max)
@@ -293,16 +300,16 @@ inner = function(paired_df,nodes){
 ##Determine various statistics. 
 ###################################################################################################################
 '%!in%' <- function(x,y)!('%in%'(x,y))
-rm = fread("E:\\research\\RatingCurveAnalysis\\stats\\removeTheseV5.csv")
+rm = fread("path\\to\\redundant\\removeTheseV5.csv")
 out = out[out$Sttn_Nm%!in%rm$Sttn_Nm,]
-path = "E:\\research\\RatingCurveAnalysis\\obs\\SWORD\\combined\\"
+path = "path/to/crossection/widths/"
 files = paste0(path, "Gauge__", out$Sttn_Nm, ".csv")
-gagePath = "E:\\research\\GlobalGaugeData\\combined\\"
+gagePath = "path\\to\\gauge\\data\\"
 gages  = gsub("grdc", "GRDC", out$Sttn_Nm)
 gageFiles = paste0(gagePath, gages, ".csv")
-outpath = paste0("E:\\research\\RatingCurveAnalysis\\FilledRecords\\allobs_wFlagsV6\\", 
+outpath = paste0("path\\out\\allobs_wFlagsV6\\", 
                  gages, ".csv")
-outpathVal = paste0("E:\\research\\RatingCurveAnalysis\\PerformanceCheckV6\\", 
+outpathVal = paste0("path\\out\\PerformanceCheckV6\\", 
                     gages, ".csv")
 
 tab = list()
@@ -313,8 +320,6 @@ for(i in 1:nrow(out)){
   output = outer(files[i], gageFiles[i])
   nds = output[[2]]
   index = which(nds==node)
-  #####Double check inner function to ensure that no duplicates are made and that performance is the same. 
-  ###############
   inside = inner(output[[1]], output[[2]][index])
   cal = inside[[1]]
   val = inside[[2]]
@@ -379,220 +384,5 @@ for(i in 1:nrow(out)){
 }
 ry = unlist(tab)
 t = out[1:length(ry),]
+##Are any different than ratingcurveparallel6? Should be zero. 
 t = t[-which(ry),]
-
-#########################################################################
-##Investigate if there are any changes. 
-#########################################################################
-path = "E:\\research\\RatingCurveAnalysis\\FilledRecords\\allobsV4\\"
-files = list.files(path, full.names=TRUE)
-filesShort = list.files(path)
-filesShort = gsub(".csv", "", filesShort)
-
-'%!in%' <- function(x,y)!('%in%'(x,y))
-rm = fread("E:\\research\\RatingCurveAnalysis\\stats\\removeTheseV3.csv")
-files = files[filesShort%!in%rm$Sttn_Nm]
-
-settingUp = function(f){
-  df = fread(f)
-  gage_only = df[Q>0&!is.na(Q)]
-  site = df$Sttn_Nm[1]
-  rc = df
-  rc$discharge = rc$Q
-  rc$Q = fifelse(is.na(rc$Q)|rc$Q<0, rc$RC, rc$Q)
-  rc$Q = fifelse(!is.na(rc$Q)&rc$Q>0,rc$Q, rc$discharge)
-  rc$Q[rc$Q<=0]= NA
-  
-  if(nrow(gage_only)>0&nrow(rc)>1){
-  workingRecord = gage_only
-  filledRecord = rc
-  #gage_only$week= week(gage_only$Date)
-  gage_only$year = year(gage_only$Date)
-  meanFlow = mean(gage_only$Q, na.rm=TRUE)
-  gage_only = gage_only[,mean(Q, na.rm=TRUE), by=(year)]#by=list(week,year)]
-  gage_only$Q = gage_only$V1
-  gage_only$meanFlow = meanFlow
-  gage_only = gage_only[!is.na(gage_only$year),]#$week),]
-  gage_only$Date = gage_only$year#MMWRweek2Date(gage_only$year, gage_only$week)
-  
-  
-  ##with rating curve
-  rc$day = format(rc$Date, "%j")
-  #rc$week = week(rc$Date)
-  rc$year = year(rc$Date)
-  meanFlowRC = mean(rc$Q, na.rm=TRUE)
-  rcurve = rc[,mean(RC, na.rm=TRUE), by=(year)]#list(week,year)]
-  
-  rc = rc[,mean(Q, na.rm=TRUE), by=(year)]#list(week,year)]
-  rc$Q = rc$V1
-  rc$RC = rcurve$V1
-  rc$meanFlow = meanFlowRC
-  rc$Sttn_Nm = site
-  rc = rc[!is.na(rc$year),]#$week),]
-  rc$Date = rc$year#MMWRweek2Date(rc$year, rc$week)
-  return(list(gage_only, rc, workingRecord, filledRecord))
-  }
-}
-
-processing = function(f){
-  gage = as.data.frame(f[[1]])
-  gage = gage[order(gage$Date),]
-  rc = as.data.frame(f[[2]])
-  rc = rc[order(rc$Date),]
-  if(nrow(gage)>3&nrow(rc)>3&length(na.omit(rc$Q))>0){
-    qg=gage$Q
-    a = pwmk(qg)
-    gage_p = a[4]#a$p.value
-    gage_s = a[2]
-    qr=na.omit(rc$Q)
-    b = pwmk(as.vector(qr))
-    rc_p = b[4]#b$p.value
-    rc_s = b[2]
-       return(data.frame(Sttn_Nm = rc$Sttn_Nm[1], gage_p=gage_p,rc_p = rc_p, gage_sen = gage_s, rc_sen = rc_s))
-  }
-}
-
-combinedFun = function(f){
-  set = settingUp(f)
-  output = processing(set)
-  return(output)
-}
-
-##Run code. 
-library(MMWRweek)
-library(modifiedmk)
-#tab = lapply(files,combinedFun)
-
-tab = list()
-for(i in 1:length(files)){
-  print(i)
-  tab[[i]] = combinedFun(files[i])
-}
-
-##Filter out based on p.value. 
-t = rbindlist(tab)
-fwrite(t, "E:\\research\\RatingCurveAnalysis\\stats\\mktestV5.csv")
-
-t = fread("E:\\research\\RatingCurveAnalysis\\stats\\mktestV5.csv")
-t$Sttn_Nm = gsub("grdc", "GRDC",t$Sttn_Nm)
-p = 0.05
-allChanging = t[t$gage_p<p|t$rc_p<p,]
-filt = t[t$gage_p>p&t$rc_p<p,]
-notObs = t[t$gage_p<p&t$rc_p>p,]
-visual = filt
-
-gage_file = st_read("E:\\research\\GlobalGaugeData\\Stations\\LocationsGaugesGRWLfilt.shp")
-#gage_file = gage_file[gage_file$agency=="GRDC",]
-#gage_file$Site_number = gage_file$Statn_Nm
-#gage_stats_vals = merge(gage_file, group,by = 'Sttn_Nm')
-gage_stats_vals = merge(gage_file, visual,by="Sttn_Nm")
-gage_stats_vals = distinct(gage_stats_vals, gage_stats_vals$Sttn_Nm, .keep_all=TRUE)
-#gage_stats_vals$diff = gage_stats_vals$rc_slope - gage_stats_vals$gage_slope
-#gage_stats_vals = gage_stats_vals[gage_stats_vals$GRWL_wd>=100,]
-mdls = match(gage_stats_vals$Sttn_Nm, out$Sttn_Nm)
-gage_stats_vals$model = out$model[mdls]
-
-library(tmap)
-tmap_mode("view")
-tm_basemap("Esri.WorldImagery")+
-tm_shape(gage_stats_vals)+
-  #tm_bubbles(col = "GRWL_wd",breaks=c(0,100,300,500,1000,3000),size=0.005)
-  tm_bubbles(col="rc_sen", breaks=c(-10,0,10), size=0.005)
-
-
-invest = function(f){
-  check = paste0(path, f, ".csv")
-  df = settingUp(check)
-  return(df)
-  # df = fread(check)
-  # gage_only = df[Q>0&!is.na(Q)]
-  # site = df$Sttn_Nm[1]
-  # rc = df
-  # rc$Sttn_Nm = site
-  # rc$discharge = rc$Q
-  # rc$Q = ifelse(is.na(rc$Q)|rc$Q<0, rc$RC, rc$Q)
-  # rc$Q = ifelse(!is.na(rc$Q)&rc$Q>0,rc$Q, rc$discharge)
-  # rc$Q[rc$Q<0]= NA
-  # return(list(gage_only, rc))
-}
-
-##Show George these locations and in GEE. 
-#08331510_USGS: Rio Grande Q decrease over time. 
-#3627001_GRDC: Gradual Q increase Amazon. 
-#2646200_GRDC: sedimentation/issue with channel bar. 
-#2260700_GRDC: sedimentation
-#2182250_GRDC: dam construction?
-#2182150_GRDC: dam construction downstream/dredging?
-#2912602_GRDC: increased wetlands around river. 
-#15490000_ANA: example of channel bar influence. 
-#09531850_USGS: decreasing flow in SW USA. 
-#09379910_USGS: Lake Powell. 
-#2588551_GRDC: increased Q. 
-
-filt$model = out$model[match(filt$Sttn_Nm, out$Sttn_Nm)]
-ggplot(filt)+geom_bar(aes(x=model), stat="count")
-
-
-st = "Y.1_RID"
-ryan = invest(st)
-nd = out[out$Sttn_Nm==st,]
-gage = ryan[[3]]
-gage = gage[order(gage$Date),]
-record = ryan[[4]]
-record = record[order(record$Date),]
-plot(record$Date, record$Q, col="red")
-lines(gage$Date, gage$Q)
-aggRC = ryan[[2]]
-aggRC = aggRC[order(aggRC$Date),]
-aggGage = ryan[[1]]
-aggGage = aggGage[order(aggGage$Date),]
-plot(aggRC$Date, aggRC$Q, col="red", type="l")
-points(aggGage$Date, aggGage$Q)
-plot(aggRC$Date, aggRC$RC, type="l")
-
-
-gb = gage
-gb$year = year(gb$Date)
-gb = aggregate(Q~year,gb,FUN= median)
-plot(gb$year, gb$Q, type="l")
-
-
-ts = merge(gage_file, t,by="Sttn_Nm")
-library(tmap)
-tmap_mode("view")
-tm_basemap("Esri.WorldImagery")+
-  tm_shape(ts[ts$GRWL_wd>=100,])+
-  tm_bubbles(col = "rc_slope",breaks=c(-100000,0,100000),size=0.005)
-
-
-##Assess aridity effect on Q change. 
-arid=st_read("E:\\research\\HydroAtlas\\BasinATLAS_v10_shp\\BasinATLAS_v10_lev10.shp")
-st_crs(arid) = crs(ts)
-arid_pts = st_join(ts, arid, join=st_within)
-arid_pts$aridity = NA
-arid_pts$aridity[arid_pts$ari_ix_sav<20] = "Arid"
-arid_pts$aridity[arid_pts$ari_ix_sav>=20&arid_pts$ari_ix_sav<50] = "Semiarid"
-arid_pts$aridity[arid_pts$ari_ix_sav>=50&arid_pts$ari_ix_sav<65] = "Subhumid"
-arid_pts$aridity[arid_pts$ari_ix_sav>=65] = "Humid"
-arid_pts = arid_pts[!is.na(arid_pts$aridity),]
-
-arid_tall = arid_pts[arid_pts$rc_p<p&arid_pts$gage_p>p,]
-arid_tall$slope = arid_tall$rc_sen
-arid_tall$type = "RC"
-arid_tall1 = arid_tall
-arid_tall1$slope = arid_tall1$gage_sen
-arid_tall1$type = "Gauge"
-arid_tall = rbind(arid_tall, arid_tall1)
-
-
-library(wesanderson)
-pal = wes_palette("Zissou1", length(unique(arid_tall$aridity)), "continuous")
-arid_tall$aridity = factor(arid_tall$aridity, levels=c("Arid", 'Semiarid', 'Subhumid','Humid'))
-
-ggplot(arid_tall)+geom_boxplot(aes(y=slope, fill = aridity))+
-  coord_cartesian(ylim=c(-.02,.02))+
-  facet_wrap(~type)+
-  scale_fill_manual(values=rev(pal))+
-  theme_classic()
-
-
